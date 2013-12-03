24d9799fd966c3bfa11a80207f3c97f9aac7d6ae
Olivier Desenfans <desenfans.olivier@gmail.com>
2013-12-03 23:20:49 +0100

    DL frame: added documentation.

--- dl_frame.h
+++ dl_frame.h
@@ -8,16 +8,19 @@
 
 #include "i2c_frames.h"
 
+/**
+ * @brief Structure representing the data frames sent by the experiment to the service module.
+ */
 typedef struct {
-    unsigned char sync[2];
-    unsigned char msgID;
-    unsigned char msgCnt;
-    unsigned char time[4];
-    TemperatureData_s temperature[3];
-    PressureData_s    pressure; 
-    unsigned char reserved[4]; 
-    unsigned char csm[2]; 
-    unsigned char crc[2]; 
+    unsigned char sync[2];              /**< @brief Synchronization bytes. */
+    unsigned char msgID;                /**< @brief Message ID. */
+    unsigned char msgCnt;               /**< @brief Message counter. */
+    unsigned char time[4];              /**< @brief Time at which the message was sent. */
+    TemperatureData_s temperature[3];   /**< @brief Temperature sensors data. */
+    PressureData_s    pressure;         /**< @brief Pressure sensor data. */
+    unsigned char reserved[4];          /**< @brief Padding bits. */
+    unsigned char csm[2];               /**< @brief Checksum bytes. */ 
+    unsigned char crc[2];               /**< @brief Cyclic redundancy checking bytes. */
 }DownlinkFrame_s;
 
 #endif  /* DEF_DL_FRAME_H */
