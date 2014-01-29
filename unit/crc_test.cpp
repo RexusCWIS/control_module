98b96d691cf7fc6e6c7ef6be0547151f21a21ecb
Olivier Desenfans <desenfans.olivier@gmail.com>
2014-01-29 23:38:59 +0100

    Modified the first CRC unit test due to a change of polynomial.

--- unit/crc_test.cpp
+++ unit/crc_test.cpp
@@ -3,14 +3,24 @@
  * @brief Cyclic Redundancy Check algorithm unit testing. 
  */
 
-#include "crc.h"
+#include "libs/crc.h"
 
-#include "gtests/gtest.h"
+#include "gtest/gtest.h"
 
-static uint8_t const data[24] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
-                                       15, 16, 17, 18, 19, 20, 21, 22, 23}; 
+static unsigned char const data[24] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                                 15, 16, 17, 18, 19, 20, 21, 22, 23}; 
 
-TEST_F(CRCTest, CRCGeneration) {
-    EXPECT_EQ(crc8(data, 24), DD);  
+TEST(CRCTest, CRCGeneration) {
+    EXPECT_EQ(crc(data, 24), 0x346B);  
 }
+/**
+ * @brief Main unit tests control function. 
+ * @details Launches the unit tests and controls the output formats. 
+ */
+int main(int argc, char **argv) {
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS(); 
+}
+
 
