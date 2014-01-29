6604138c5c6c331ad293fd3cbe118fee16c52a45
Olivier Desenfans <desenfans.olivier@gmail.com>
2014-01-29 18:08:00 +0100

    CRC table generator: First implementation.
    Computes and prints a CRC table for the given polynomial in the given
    output file.
    
    The code does not provide a valid CRC table yet.

--- utils/crc_table_generator/crc_table_generator.pl
+++ utils/crc_table_generator/crc_table_generator.pl
@@ -3,12 +3,66 @@
 use strict;
 use warnings;
 
-my ($polynomial) = @ARGV;
+# Width of the CRC polynomial 
+my $width    = 16;
+# Top bit index of a $width bit wide variable
+my $top_bit  = (1 << ($width - 1));
+print "Top bit: $top_bit\n"; 
+# 16-bit binary mask 
+my $bit_mask = 0xFFFF;  
+
+my ($polynomial, $file) = @ARGV;
 if($polynomial =~ m/0x/) {
     $polynomial =~ s/0x(.*)/$1/;
     $polynomial = hex($polynomial);
 }
 
-$polynomial += 2; 
 print "Polynomial: $polynomial\n"; 
+
+my @crc_table = (); 
+
+for(my $dividend = 0; $dividend < 256; $dividend++) {
+    
+    my $remainder = $dividend << ($width - 8);
+    
+    for(my $bit = 8; $bit > 0; $bit--) {
+        
+        if(($remainder & $top_bit) != 0) {
+            $remainder = (($remainder << 1) & $bit_mask) ^ $polynomial;
+        }
+        
+        else {
+            $remainder = (($remainder << 1) & $bit_mask);
+        }
+    }
+    
+    $crc_table[$dividend] = $remainder; 
+}
+
+# Write data to the output file
+
+# Create the output file
+open( OUTFD, '>>', $file )
+    or die("Couldn't create file $file: $!");      # Create the output file
+open( OUTFD, '>', $file );
+
+my $header = "/* Automatically generated file: do not modify.\n * CRC polynomial: $ARGV[0].\n */";
+
+# Print comment header.
+print OUTFD $header, "\n\n"; 
+
+# Print CRC table as a C array.
+my $line_counter = 8;
+
+print OUTFD 'const uint8_t crc_table[256] = {', "\n\t"; 
+for(my $index = 0; $index < 256; $index++) {
+    printf OUTFD "0x%04xu, ", $crc_table[$index];
+    $line_counter--;
+
+    if(($line_counter == 0) && ($index < 256)) {
+        print OUTFD "\n\t"; 
+        $line_counter = 8; 
+    }
+}
+print OUTFD "\n};";
 
