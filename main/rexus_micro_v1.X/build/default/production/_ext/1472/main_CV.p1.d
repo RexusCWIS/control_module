80051176f3e79502b02201db9377d2aafd134dd5
Olivier Desenfans <desenfans.olivier@gmail.com>
2014-01-29 16:54:31 +0100

    Deleted the build files from git.

--- main/rexus_micro_v1.X/build/default/production/_ext/1472/main_CV.p1.d
+++
@@ -1,7 +0,0 @@
- build/default/production/_ext/1472/main_CV.d  \
- build/default/production/_ext/1472/main_CV.p1:  \
- ../main_CV.c  \
-../settings.h  \
-../../drivers/i2c_slave.h  \
-../../config/pic18f2520_config.h  \
-../i2c_frames.h 
