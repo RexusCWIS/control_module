b16a6b1cd42d35b75605ca53849bed039109ff77
Olivier Desenfans <desenfans.olivier@gmail.com>
2014-01-30 11:20:09 +0100

    CRC generation: code cleaning.
    Added an INITIAL_REMAINDER macro, changed the name of an increment variable.

--- libs/crc.h
+++ libs/crc.h
@@ -11,19 +11,19 @@
 typedef uint16_t crc_t;
 
 #define WIDTH   (8 * sizeof(crc_t))
+#define INITIAL_REMAINDER   0xFFFFu
 
 extern const crc_t crc_table[256]; 
 
 static inline crc_t crc(uint8_t const msg[], uint8_t size) {
 
     uint8_t data; 
-    crc_t remainder = 0; 
+    crc_t remainder = INITIAL_REMAINDER; 
 
-    for(uint8_t byte = 0; byte < size; byte++) {
+    for(uint8_t index = 0; index < size; index++) {
         
-        data = msg[byte] ^ (remainder >> (WIDTH - 8)); 
+        data = msg[index] ^ (remainder >> (WIDTH - 8)); 
         remainder = crc_table[data] ^ (remainder << 8); 
-
     }
     return remainder; 
 }
