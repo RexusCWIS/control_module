8a1f486f264a51bc003570b720a7e66bc909dcf1
Olivier Desenfans <desenfans.olivier@gmail.com>
2014-01-30 11:21:23 +0100

    CRC unit tests: added two test cases.
    Test case 1 compares the generated table with a table coming from
    open source software, using the CRC-CITT standard.
    
    Test case 2 compares the outputs given by our CRC algorithm and
    the open source one.
    
    Test case 3 verifies that, for a perfect transmission (no difference
    between the sent and received messages), the CRC is zero.

--- unit/crc_test.cpp
+++ unit/crc_test.cpp
@@ -5,13 +5,33 @@
 
 #include "libs/crc.h"
 
+#include "crc16.h"
 #include "gtest/gtest.h"
 
-static unsigned char const data[24] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-                                 15, 16, 17, 18, 19, 20, 21, 22, 23}; 
+static unsigned char data[26] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                                 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 0}; 
 
+
+TEST(CRCTest, TableGeneration) {
+    for(int index = 0; index < 256; index++) {
+        ASSERT_EQ(crc_table[index], crc16_tbl[index]);  
+    }
+}
+
+/**
+ * @brief Compares the results between an open source CRC algorithm and ours. 
+ */
 TEST(CRCTest, CRCGeneration) {
-    EXPECT_EQ(crc(data, 24), 0x346B);  
+    EXPECT_EQ(crc(data, 24), crc16_block(0xFFFFu, data, 24));     
+}
+
+TEST(CRCTest, CRCTransmission) {
+
+    uint16_t c = crc(data, 24); 
+    data[24]   = (uint8_t) (c >> 8);
+    data[25]   = (uint8_t) (c & 0xFFu); 
+
+    EXPECT_EQ(crc(data, 26), 0);
 }
 /**
  * @brief Main unit tests control function. 
