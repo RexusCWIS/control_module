4866a310e81110c4f3b545e9fae5180a8f6d0d48
Olivier Desenfans <desenfans.olivier@gmail.com>
2014-02-12 10:23:19 +0100

    Drivers: added functions to read and write bytes from/to data EEPROM.
    drivers/eeprom.h
    drivers/eeprom.c
        Implemented the eeprom_read_byte and eeprom_write_byte functions.

---
+++ drivers/eeprom.c
@@ -0,0 +1,45 @@
+/**
+ * @file eeprom.c
+ * @brief Implementation of data EEPROM access functions.
+ */
+
+#include "drivers/eeprom.h"
+
+uint8_t eeprom_read_byte(uint8_t address) {
+
+    EEADR = address;
+    /* Access data EEPROM */
+    EECON1bits.EEPGD = 0;
+    /* Initiate read */
+    EECON1bits.RD    = 1;
+
+    /* Data is available the next cycle in EEDATA */
+    return EEDATA;
+}
+
+
+
+void eeprom_write_byte(uint8_t data, uint8_t address) {
+    
+    EEADR  = address;
+    EEDATA = data;
+
+    EECON1bits.EEPGD = 0;
+    EECON1bits.CFGS  = 0;
+    EECON1bits.WREN  = 1;
+
+    /* Initiation of the write sequence */
+    di();
+    EECON2 = 0x55u;
+    EECON2 = 0xAAu;
+    EECON1bits.WR = 1;
+    ei();
+
+    /* Wait for the completion of the write operation */
+    while(!PIR2bits.EEIF)
+        ;
+
+    /* Reset interrupt and write enable bits */
+    PIR2bits.EEIF   = 0;
+    EECON1bits.WREN = 0;
+}
---
+++ drivers/eeprom.h
@@ -0,0 +1,36 @@
+/**
+ * @file eeprom.h
+ * @brief Data EEPROM access functions.
+ */
+
+#ifndef DEF_EEPROM_H
+#define DEF_EEPROM_H
+
+#include <htc.h>
+#include <stdint.h>
+
+/**
+ * @brief Reads a @p data byte from the given @p address in the data EEPROM.
+ * @param[in] address   Address from which the data must be read from.
+ * @returns The @p data byte at the given @p address.
+ * @see eeprom_write_byte()
+ */
+uint8_t eeprom_read_byte(uint8_t address);
+
+/**
+ * @brief Writes the given @p data byte to the given @p address in the data EEPROM.
+ * @details Initiates a write sequence to the data EEPROM. This function returns
+ *          once the write operation has been completed. A critical section is
+ *          entered during the execution of this function as a specific sequence
+ *          must be written to initiate the write operation. 
+ * @param[in] data      Byte to be written in data EEPROM.
+ * @param[in] address   Address at which the byte must be written.
+ * @note This function does not perform any write verification. The user must
+ *       perform it himself by calling the @ref eeprom_read_byte function
+ *       and compare the data in memory with the data byte in RAM.
+ * @see eeprom_read_byte()
+ */
+void eeprom_write_byte(uint8_t data, uint8_t address);
+
+#endif /* DEF_EEPROM_H */
+        
